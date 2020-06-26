76485d0078e37fc73818dca1b0a57a0f40e56860
lordjuey <juwon.dol@gmail.com>
2020-06-27 01:32:47 +1000

    more javascript added to get response from API

--- script.js
+++ script.js
@@ -1,9 +1,37 @@
 //to create function that append city names to the side bar
+
 $("#submitBtn").on("click", function () {
   event.preventDefault();
-  console.log($("#cityName").val()); // to test its grabbing the right input
+  const city = $("#cityName").val(); //!!!! have to clean this up
+  console.log(city); // to test its grabbing the right input
   const a = $("<div>");
   a.addClass("cityEl");
-  a.text($("#cityName").val());
+  a.text(city);
   $(".cityLists").append(a);
+});
+
+//to write a function that gets info from openWeatherMap API
+
+$(".cityLists").on("click", function (event) {
+  event.preventDefault();
+
+  cityName = $("#cityName").val();
+  const queryURL =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    cityName +
+    "&appid=166a433c57516f51dfab1f7edaed8413";
+  // AJAX call to the OpenWeatherMap API
+
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  })
+    // We store all of the retrieved data inside of an object called "response"
+    .then(function (response) {
+      // Log the queryURL
+      console.log(queryURL);
+
+      // Log the resulting object
+      console.log(response);
+    });
 });
